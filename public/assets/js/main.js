9348ffbeec2be5ae696661efde5efc7c156c440e
T-Rekt <t.to.the.rekt@gmail.com>
2020-10-22 22:32:54 +0700

    Check winner

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,7 +5,8 @@ new Vue({
     status: [],
     problemStr: '',
     updateStatus: 'Idle',
-    nextUpdate: 1
+    nextUpdate: 1,
+    winner: null
   },
   // computed: {
   //   problemStr() { 
@@ -30,6 +31,99 @@ new Vue({
         }
       });
     },
+    deepClone(obj) {
+      return JSON.parse(JSON.stringify(obj));
+    },
+    checkWinner(board) {
+      // Check rows
+      for (let i = 0; i < board.length; i++) {
+        let count = 0;
+        let curr = board[i][0];
+
+        for (let j = 0; j < board[i].length; j++) {
+          if (board[i][j] !== curr) count = 0;
+
+          curr = board[i][j];
+
+          count++;
+        }
+
+        if (count == 3 && curr) return curr;
+      }
+
+      // Check cols
+      for (let j = 0; j < board.length; j++) {
+        let count = 0;
+        let curr = board[0][j];
+
+        for (let i = 0; i < board[j].length; i++) {
+          if (board[i][j] !== curr) count = 0;
+
+          curr = board[i][j];
+
+          count++;
+        }
+
+        if (count == 3 && curr) return curr;
+      }
+
+      let count = 0;
+      let curr = board[0][0];
+
+      for (let i = 0; i < board.length; i++) {
+
+        if (board[i][i] !== curr) count = 0;
+
+        curr = board[i][i];
+
+        count++;
+      }
+
+      if (count == 3 && curr) return curr;
+
+      count = 0;
+      curr = board[1][1];
+
+      for (let i = 0; i < board.length; i++) {
+        if (board[i][board.length - i - 1] !== curr) count = 0;
+
+        curr = board[i][i];
+
+        count++;
+      }
+
+      if (count == 3 && curr) return curr;
+
+      return null;
+    },
+    stats(judgeResult) {
+      let clonedJudgeResult = this.deepClone(judgeResult).map((item, i) => {
+        item.index = i;
+
+        return item;        
+      });
+
+      let board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+      ];
+
+      clonedJudgeResult.sort((a, b) => a.submissionTime - b.submissionTime);
+
+      for (let move of clonedJudgeResult) {
+        let row = ~~(move.index / 3);
+        let col = move.index % 3;
+
+        board[row][col] = move.user;
+
+        let winner = this.checkWinner(board);
+
+        if (winner !== null) return winner;
+      }
+
+      return null;
+    },
     async update() {
       this.updateStatus = 'Updating...';
 
@@ -40,6 +134,8 @@ new Vue({
       let judgeResult = await this.judge(this.problemStr, this.startTime, this.participant1, this.participant2);
 
       this.status = judgeResult.data.data;
+
+      this.winner = this.stats(this.status);
 
       this.updateStatus = 'Idle';
     },
