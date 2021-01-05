ab423be297924a52db487e4419fc43ffb3c04068
T-Rekt <t.to.the.rekt@gmail.com>
2021-01-05 17:45:25 +0700

    Fix missing group id in updateProblemList

--- updateProblemList.js
+++ updateProblemList.js
@@ -3,14 +3,14 @@ let fs = require('fs');
 
 let group = 'FLVn1Sc504';
 
-let getOIContests = async() => {
-  let contests = await cf.getGroupContestsList();
+let getOIContests = async(group) => {
+  let contests = await cf.getGroupContestsList(group);
 
   return contests.filter(contest => contest.name.includes('OI set'));
 }
 
-let getOIProblems = async() => {
-  let oiContests = await getOIContests();
+let getOIProblems = async(group) => {
+  let oiContests = await getOIContests(group);
 
   let problemSets = await Promise.all(oiContests.map(contest => cf.getProblemsListInGroupContest(group, contest.contestId)));
 
@@ -18,7 +18,7 @@ let getOIProblems = async() => {
 }
 
 (async() => {
-  let oiProblems = await getOIProblems();
+  let oiProblems = await getOIProblems(group);
 
   fs.writeFileSync('./problems.json', JSON.stringify(oiProblems));
 })();
